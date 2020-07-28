b8125691a8c53c751e2576f8b8ecf1c31e480753
* <haridarshanofficial@gmail.com>
2020-07-28 18:52:49 +0530

    Added django-heroku settings

--- Myblog/settings.py
+++ Myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('email')
 EMAIL_HOST_PASSWORD = os.environ.get('pass')
 EMAIL_PORT = 587
+
+django_heroku.settings(locals())
